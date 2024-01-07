2c932acc3d64edf12b0b5680d53a43b21edee81f
sgnoya <naoya.sogi@gmail.com>
2024-01-07 13:20:17 +0900

    elsevier: use selenium

--- elsevier.py
+++ elsevier.py
@@ -5,8 +5,22 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from omegaconf import OmegaConf
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 from utils import discord, init_twitterapi
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument('window-size=1400,600')
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--user-agent=Chrome/110")
+options.add_experimental_option('prefs', {
+    'credentials_enable_service': False,
+    'profile': {'password_manager_enabled': False}
+})
 
 cwd, _ = os.path.split(os.path.abspath(__file__))
 keys = OmegaConf.load(os.path.join(cwd, "keys.yml"))
@@ -24,22 +38,28 @@ with open(os.path.join(cwd, "elsevier.csv"), "r") as f:
 
 sent = []
 
+# %%
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(5)
+
+# %%
 for journal in journals:
-    response = requests.get(
-        "https://www.journals.elsevier.com/" + journal + "/recent-articles"
-    )
-    soup = BeautifulSoup(response.content.decode(), "html.parser")
-    docs = soup.find_all("article")
+    url = "https://www.sciencedirect.com/journal/" + journal + "/articles-in-press"
+    print(url)
+    driver.get(url)
+    time.sleep(5)
+    elem = driver.find_elements(By.CSS_SELECTOR, ".text-m.u-font-serif.u-display-inline")
+    papers = []
+    for e in elem:
+        soup = BeautifulSoup(e.get_attribute("innerHTML"), "html.parser")
+        title = e.find_element(By.CLASS_NAME, "js-article-title").text.replace("\n", " ")
+        papers.append([title, "https://www.sciencedirect.com"+ soup.find("a").get("href")])
+
     msg = "@here (Elsevier)" + journal + "\n"
     discord(keys.discord, msg)
-    time.sleep(2)
 
     msg = ""
-    for i, doc in enumerate(docs):
-        meta = doc.find("a")
-        title = meta.text
-        link = meta.get("href")
-
+    for i, (title, link) in enumerate(papers):
         if title + "\n" not in data:
             msg += title + "\n" + link + "\n"
             sent.append(title + "\n")
@@ -48,18 +68,13 @@ for journal in journals:
             _msg += title + "\n" + link + "\n"
             try:
                 twapi.update_status(_msg)
-                time.sleep(0.1)
             except Exception as e:
                 print(e)
+                print(journal, title, link)
 
-        if (i + 1) % 9 == 0:
-            discord(keys.discord, msg + "\n")
-            time.sleep(2)
-            msg = ""
-
-    discord(keys.discord, msg + "\n")
-    time.sleep(2)
 
 
 with open(os.path.join(cwd, "elsevier.csv"), "a") as f:
     f.writelines(sent)
+
+driver.quit()
